824cf0f5808fb02273ead640ed3202d071930552
maristelcornesio <126033823+maristelcornesio@users.noreply.github.com>
2023-03-03 09:36:04 +0800

    Add files via upload

---
+++ MY PRODUCT.js
@@ -0,0 +1,14 @@
+const productitem = {
+        "product": [{
+            "name": "iphone 7",
+            "brand": "apple",
+            "decription": "iphone 7",
+            "price": "13000",
+            "image":"IP 7.jfif",
+            "command":"add to cart",
+            "id": "1"
+
+            }
+        ]
+       
+}
